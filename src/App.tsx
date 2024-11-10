2956fdf12cf96a74710163e6f53a977dc8a44b91
Cristina Ayala <crisstinaayala777@gmail.com>
2024-11-10 09:20:02 -0800

    Finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Alert from "./components/Alert";
 import Button from "./components/Button";
+import { useState } from "react";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Button>My button</Button>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>My button</Button>
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 interface Props {
-    children: string
+    children: string;
+    color?: string;
+    onClick: () => void;
 }
 
-const Button = ({ children }: Props) => {
+const Button = ({ children, onClick, color = 'primary' }: Props) => {
   return (
-    <button className='btn btn-primary'>{children}</button>
+    <button className={'btn btn-' + color} onClick={onClick}>{children}</button>
   )
 }
 
