293740b0434b2aa0a4ef5c4a1c1ee357a1b1cbc2
Cristina Ayala <crisstinaayala777@gmail.com>
2024-11-10 09:04:38 -0800

    new stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+      <Button>My button</Button>
     </div>
   );
 }
